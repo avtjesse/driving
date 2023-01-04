40908ef1ad5e6362561226a1f6645f002717a01c
Lin Tsao Hsiang <58588294+avtjesse@users.noreply.github.com>
2023-01-04 17:51:48 +0800

    add Taiwan

---
+++ driving.py
@@ -0,0 +1,8 @@
+country = ('please input your country: ')
+age = ('please input your age: ')
+age = int(age)
+if country == 'Taiwan':
+	if age >= 18:
+		print('you can have a driving licence')
+	else:
+		print('you cannot have a driving licence yet')
